23bc1de068e16794fe53d4c790eb9fee12b77ee2
Efe Giray <efe_giray@alyo.com.tr>
2018-03-19 17:18:55 +0300

    api updates - sync opencart

--- dia.php
+++ dia.php
@@ -1,32 +1,38 @@
 <?php
 class Dia { 
-	const DIA_API = 'http://diademo.dia.gen.tr';
 	const DIA_SIS = '/api/v3/sis/json';
 	const DIA_SCF = '/api/v3/scf/json';
 
 	private $session_token = '';
 	private $company_code = '';
+	private $dia_server = '';
+	private $dia_user = '';
+	private $dia_password = '';
 
 	private $disconnect_same_user = 'False';
 	private $language = 'tr';
 
-	public function __construct($disconnect_same_user = 'False', $language = 'tr') { 
+	public function __construct($dia_api_settings, $disconnect_same_user = 'False', $language = 'tr') { 
+		$this->company_code = $dia_api_settings['module_dia_company_code'];
+		$this->dia_server = $dia_api_settings['module_dia_server'];
+		$this->dia_user = $dia_api_settings['module_dia_user'];		
+		$this->dia_password = $dia_api_settings['module_dia_password'];
+
 		$this->disconnect_same_user = $disconnect_same_user;
 		$this->language = $language;
 	}
 
-	public function login($username, $password, $company_code = 0) {
+	public function login() {
 		$request = new stdClass();
 		$request->login = new stdClass();
-		$this->company_code = $company_code;
 
-		$request->login->username = $username;
-		$request->login->password = $password;
+		$request->login->username = $this->dia_user;
+		$request->login->password = $this->dia_password;
 		$request->login->lang = $this->language;
 		$request->login->disconnect_same_user = $this->disconnect_same_user;
 
 		$request_body = json_encode($request);
-		$request_url = Dia::DIA_API . Dia::DIA_SIS;
+		$request_url = $this->dia_server . Dia::DIA_SIS;
 
 		$response = $this->curl_api($request_url, $request_body);
 
@@ -42,27 +48,30 @@ class Dia {
 	public function list_products($period, $filters = array(), $list_limit = 100, $offset = 0) {
 		$request = new stdClass();
 		$request->scf_stokkart_listele = new stdClass();
-		$request->scf_stokkart_listele->limit = $list_limit;
-		$request->scf_stokkart_listele->offset = $offset;
 
 		$request->scf_stokkart_listele->donem_kodu = $period;
 		$request->scf_stokkart_listele->session_id = $this->session_token;
-		$request->scf_stokkart_listele->firma_kodu = $this->company_code;
+		$request->scf_stokkart_listele->firma_kodu = (int)$this->company_code;
+
+		$request->scf_stokkart_listele->limit = $list_limit;
+		$request->scf_stokkart_listele->offset = $offset;
 
 		$request_body = json_encode($request);
-		$request_url = Dia::DIA_API . Dia::DIA_SCF;
+		$request_url = $this->dia_server . Dia::DIA_SCF;
 
 		$response = $this->curl_api($request_url, $request_body);
 
 		if ($response['code'] == 200) {
 			$response = $response['result'];
+		} else {
+			var_dump($response);
+			$response = array();
 		}
 
 		return $response;
 	}
 
 	public function curl_api($url, $body, $options = array()) {
-		echo $body . PHP_EOL;
 		$curl = curl_init();
 
 		curl_setopt($curl, CURLOPT_URL, $url);
--- main.php
+++ main.php
@@ -2,36 +2,40 @@
 include(__DIR__ . '/dia.php');
 
 $config = json_decode(file_get_contents(__DIR__ . '/config.json'), true);
-
-$dia = new Dia(false, 'tr');
-
-if (!$dia->login('demo', 'demo', 34)) {
-	exit('Unable to login');
-}
-
 $mysqli = new mysqli($config['mysql_host'], $config['mysql_user'], $config['mysql_password'], $config['mysql_db']);
 
+$query = $mysqli->prepare('SELECT `key`, `value` FROM setting WHERE code = "module_dia"');
+$query->execute();
+$result = $query->get_result();
+
+$dia_api_settings = array();
+
+while ($row = $result->fetch_assoc()) {
+	$dia_api_settings[$row['key']] = $row['value'];
+}
+
+$dia = new Dia($dia_api_settings, 'False', 'tr');
+
+$start = microtime(true);
+if (!$dia->login()) {
+	exit('Unable to login');
+}
+echo 'Logged in successfully in: ' . (microtime(true) - $start) . PHP_EOL;
 // Check connection
 if (mysqli_connect_errno()) {
 	exit("Failed to connect to MySQL: " . mysqli_connect_error());
 }
 
 $opencart_products = array();
+$start = microtime(true);
 $products = $dia->list_products(1, array(), 3);
+echo 'Listed products successfully in: ' . (microtime(true) - $start) . PHP_EOL;
 
 foreach ($products as $product) {
-	$opencart_products[] = array (
-		'sku' => $product['stokkartkodu'],
-		'mpn' => $product['_key'],
-		'title' => $product['aciklama'],
-		'stock' => $product['fiili_stok'],
-		'price' => $product['fiyat1']
-	);
-
 	//check if product already exists
+	$start = microtime(true);
 	$query = $mysqli->prepare('SELECT product_id, quantity FROM product WHERE mpn = ? LIMIT 1');
 	$query->bind_param('s', $product['_key']);
-	echo $product['_key'];
 
 	$query->execute();
 
@@ -40,7 +44,17 @@ foreach ($products as $product) {
 	if ($result->num_rows > 0) {
 		//this item already exists, update price and stock
 		while ($row = $result->fetch_assoc()) {
-			var_dump($row);
+			$query = $mysqli->prepare('UPDATE product SET quantity = ?, price = ?, sku = ? WHERE product_id = ? AND mpn = ? LIMIT 1');
+			$query->bind_param('isdsi', $product['fiili_stok'], $product['fiyat1'], $product['stokkartkodu'], $row['product_id'], $product['_key']);
+			$query->execute();
+
+			$query = $mysqli->prepare('UPDATE product_description SET name = ? WHERE product_id = ? LIMIT 1');
+			$query->bind_param('si', $product['aciklama'], $row['product_id']);
+			$query->execute();
+
+			if ($query->affected_rows == 1) {
+				echo $row['product_id'] . '-' . $product['_key'] . ' is synced successfully in: ' . microtime(true) - $start . PHP_EOL;
+			}
 		}
 	}
 	
